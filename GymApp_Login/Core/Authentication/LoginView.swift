7dadd4ca2fd6cb350088f1aeba1fd15bcfd9e2f3
Kyle McQuillan <kyle.mcquillan.01@gmail.com>
2024-03-20 21:45:16 +0000

    added views

--- GymApp_Login/Core/Authentication/LoginView.swift
+++ GymApp_Login/Core/Authentication/LoginView.swift
@@ -8,14 +8,68 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
         NavigationStack{
             VStack{
                 // image
+                Image("GymLogo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width:100, height: 100)
+                    .padding(.vertical,32)
                 
                 // Form Fields
                 
+                VStack(spacing:24){
+                    InputView(text: $email,
+                              title: "Email Address",
+                              placeholder: "name@example.com")
+                    .autocapitalization(.none)
+                
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "Enter your password",
+                              isSecureField: true
+                    )
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
+                
+                
+                
                 // Sign up button
+                Button{
+                    print("Log user in..")
+                } label: {
+                    HStack{
+                        Text("SIGN IN")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                }
+                .background(Color(.systemBlue))
+                .cornerRadius(10)
+                .padding(.top, 24)
+                
+                         
+                Spacer()
+                         
+                NavigationLink{
+                    RegistrationView()
+                        .navigationBarBackButtonHidden(true)
+                } label: {
+                    HStack(spacing: 3){
+                        Text("Dont have an account")
+                        Text("Sign Up")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size:14))
+                }
+            
             }
         }
     }
--- GymApp_Login/Core/Profile/ProfileView.swift
+++ GymApp_Login/Core/Profile/ProfileView.swift
@@ -9,7 +9,57 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            Section{
+                HStack{
+                    Text(User.MOCK_USER.initials)
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 72, height: 72)
+                        .background(Color(.systemGray3))
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    
+                    VStack(alignment: .leading, spacing: 4){
+                    
+                        Text(User.MOCK_USER.fullname)
+                            .fontWeight(.semibold)
+                            .padding(.top,4)
+                        
+                        Text(User.MOCK_USER.email)
+                            .font(.footnote)
+                            .foregroundColor(.gray)
+                        
+                    }
+                }
+            }
+            
+            Section("General"){
+                HStack{
+                    SettingsRowView(imageName: "gear",
+                                    title: "version",
+                                    tintColor: Color(.systemGray))
+                    Spacer()
+                    
+                    Text("1.0.0")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+            
+            Section("Account"){
+                Button{
+                    print("Sign out..")
+                } label: {
+                    SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: .red)
+                }
+                Button{
+                    print("Delete out..")
+                } label: {
+                    SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: .red)
+                }
+            }
+        }
     }
 }
 
