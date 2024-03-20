ac63bf00a41537486b5f891b971175c7f2317acf
Kyle McQuillan <kyle.mcquillan.01@gmail.com>
2024-03-20 20:25:35 +0000

    Initial Commit

---
+++ GymApp_Login/GymApp_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymApp_LoginApp.swift
+//  GymApp_Login
+//
+//  Created by Kyle on 20/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct GymApp_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
