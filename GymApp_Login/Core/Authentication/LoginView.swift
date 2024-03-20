c1f623beb830237c2ed8f6660e54ac9dfdd9eaea
Kyle McQuillan <kyle.mcquillan.01@gmail.com>
2024-03-20 20:32:25 +0000

    Initial upload of files and structure

---
+++ GymApp_Login/Core/Authentication/LoginView.swift
@@ -0,0 +1,26 @@
+//
+//  LoginView.swift
+//  GymApp_Login
+//
+//  Created by Kyle on 20/03/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationStack{
+            VStack{
+                // image
+                
+                // Form Fields
+                
+                // Sign up button
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
