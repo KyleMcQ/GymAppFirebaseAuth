3f1dfa38ce1b2ade552f422fcaf16dafbdcf36d4
Kyle McQuillan <kyle.mcquillan.01@gmail.com>
2024-03-21 17:58:59 +0000

    Home View using Live Username

--- GymApp_Login/Core/Authentication/View/HomeView.swift
+++ GymApp_Login/Core/Authentication/View/HomeView.swift
@@ -9,61 +9,65 @@ import SwiftUI
 
 struct HomeView: View {
     let username: String = "Shane McQuillan"
-    
+    @EnvironmentObject var viewModel: AuthViewModel
+
     // State variables for navigation
     @State private var shouldNavigateToAddWorkout = false
     @State private var shouldNavigateToViewWorkout = false
     @State private var shouldNavigateToProfile = false
-
+    
     var body: some View {
-        NavigationStack {
-            List {
-                Section("Workouts") {
-                    // Button for adding a workout
-                    Button {
-                        shouldNavigateToAddWorkout = true
-                    } label: {
-                        WorkoutRowView(imageName: "plus.circle.fill", title: "Add Workout", tintColor: .blue)
+        if let user = viewModel.currentUser {
+            
+            NavigationStack {
+                List {
+                    Section("Workouts") {
+                        // Button for adding a workout
+                        Button {
+                            shouldNavigateToAddWorkout = true
+                        } label: {
+                            WorkoutRowView(imageName: "plus.circle.fill", title: "Add Workout", tintColor: .blue)
+                        }
+                        .listRowBackground(Color.white)
+                        
+                        // Button for viewing workouts
+                        Button {
+                            shouldNavigateToViewWorkout = true
+                        } label: {
+                            WorkoutRowView(imageName: "eye", title: "View Workout", tintColor: .blue)
+                        }
+                        .listRowBackground(Color.white)
                     }
-                    .listRowBackground(Color.white)
                     
-                    // Button for viewing workouts
-                    Button {
-                        shouldNavigateToViewWorkout = true
-                    } label: {
-                        WorkoutRowView(imageName: "eye", title: "View Workout", tintColor: .blue)
+                    Section("Settings") {
+                        // Button for settings
+                        Button {
+                            shouldNavigateToProfile = true
+                        } label: {
+                            WorkoutRowView(imageName: "gear", title: "Settings", tintColor: Color.gray)
+                        }
+                        .listRowBackground(Color.white) 
                     }
-                    .listRowBackground(Color.white)
                 }
-                
-                Section("Settings") {
-                    // Button for settings
-                    Button {
-                        shouldNavigateToProfile = true
-                    } label: {
-                        WorkoutRowView(imageName: "gear", title: "Settings", tintColor: Color.gray)
+                .navigationTitle("Home")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Text(user.fullname)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
                     }
-                    .listRowBackground(Color.white) 
                 }
+                // Hidden navigation links that are activated by the buttons
+                .background(
+                    NavigationLink(destination: AddWorkoutView(), isActive: $shouldNavigateToAddWorkout) { EmptyView() }.hidden()
+                )
+                .background(
+                    NavigationLink(destination: ViewWorkoutView(), isActive: $shouldNavigateToViewWorkout) { EmptyView() }.hidden()
+                )
+                .background(
+                    NavigationLink(destination: ProfileView(), isActive: $shouldNavigateToProfile) { EmptyView() }.hidden()
+                )
             }
-            .navigationTitle("Home")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Text(username)
-                        .font(.subheadline)
-                        .foregroundColor(.secondary)
-                }
-            }
-            // Hidden navigation links that are activated by the buttons
-            .background(
-                NavigationLink(destination: AddWorkoutView(), isActive: $shouldNavigateToAddWorkout) { EmptyView() }.hidden()
-            )
-            .background(
-                NavigationLink(destination: ViewWorkoutView(), isActive: $shouldNavigateToViewWorkout) { EmptyView() }.hidden()
-            )
-            .background(
-                NavigationLink(destination: ProfileView(), isActive: $shouldNavigateToProfile) { EmptyView() }.hidden()
-            )
         }
     }
 }
